d09e2fdb83656956fbef8018bf8af3de8333c01e
La Duy Anh <anhld.22bi13016@usth.edu.vn>
2024-09-17 11:19:57 +0700

    Revert "fix login to main"
    This reverts commit 606fb647

--- app/src/main/java/vn/edu/usth/dropboxui/SignInActivity.java
+++ app/src/main/java/vn/edu/usth/dropboxui/SignInActivity.java
@@ -4,42 +4,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import vn.edu.usth.dropboxui.ui.home.HomeFragment;
 
 public class SignInActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) { // corrected parameter name
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
-        ImageButton imageButton = (ImageButton) findViewById(R.id.header_image_button);
-
-        imageButton.setOnClickListener(new View.OnClickListener() {
+        Button mainButton = findViewById(R.id.continue_btn);
+        mainButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(SignInActivity.this, "Back", Toast.LENGTH_LONG).show();
-                Intent intent = new Intent(SignInActivity.this, LoginActivity.class);
-                startActivity(intent);
-
-            }
-        });
-
-        Button button = (Button) findViewById(R.id.continue_btn);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(SignInActivity.this, "Log in", Toast.LENGTH_LONG).show();
-                Intent intent = new Intent(SignInActivity.this, HomeFragment.class);
+                Intent intent = new Intent(SignInActivity.this, MainActivity.class);
                 startActivity(intent);
             }
         });
     }
-
 }
