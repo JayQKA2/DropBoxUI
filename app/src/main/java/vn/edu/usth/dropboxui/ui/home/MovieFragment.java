1b8508856d0bed80d700764a8a46658215d8eb86
Minh Duy <kingloki2004@gmail.com>
2024-09-19 11:31:12 +0700

    final add ui

--- app/src/main/java/vn/edu/usth/dropboxui/ui/home/MovieFragment.java
+++ app/src/main/java/vn/edu/usth/dropboxui/ui/home/MovieFragment.java
@@ -1,22 +1,29 @@
 package vn.edu.usth.dropboxui.ui.home;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import vn.edu.usth.dropboxui.R;
-
 
 public class MovieFragment extends Fragment {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_movie, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_movie, container, false);
+
+        FloatingActionButton fab = root.findViewById(R.id.float_Bton2);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "Add movie", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return root;
     }
 }
